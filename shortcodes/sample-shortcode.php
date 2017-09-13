8fce816f4e32f68bc0975d8d95ec310ed89d1051
LAPTOP-1SS5URD1\Jun <lamsmallarijr@gmail.com>
2017-09-13 10:48:49 +0800

    added bootstrap v4

--- shortcodes/sample-shortcode.php
+++ shortcodes/sample-shortcode.php
@@ -1,12 +1,32 @@
 <?php 
 
-	function sampleShortcode() {
+	function productSlider() {
 		ob_start();
+
+		$args = array(
+            'post_type' => 'pincha_product',
+            'orderby'   => 'date',
+            'order'     => 'ASC'
+        );
+		
+		$query = new WP_Query( $args );
 	    ?>
 
+			<?php if( $query->have_posts() ) : ?>
+			    <?php while( $query->have_posts() ) : $query->the_post(); ?>
+			        <?php
+			            if (has_post_thumbnail( $post->ID ) ) {
+			                $bg_url   = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' );
+			                $bg_image = 'style="background-image: url('. $bg_url[0] .')"';
+			            } else {
+			                $bg_image = 'style="background-image: url('. wp_get_attachment_url(576) .')"';
+			            }
+			        ?>
+			    <?php endwhile; wp_reset_postdata(); ?>
+			<?php endif; ?>
 		
 	    <?php
 		return ob_get_clean();
 	}
-	add_shortcode( 'sample-shortcode' , 'sampleShortcode' );
+	add_shortcode( 'product-slider' , 'productSlider' );
 
