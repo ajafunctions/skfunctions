69b6c36f00d1828c76f34c8a4d56956b5acb77c8
LAPTOP-1SS5URD1\Jun <lamsmallarijr@gmail.com>
2018-05-02 10:41:46 +0800

    Add SmoothScroll resuable function

--- js/main.js
+++ js/main.js
@@ -673,5 +673,8 @@
 	window.getURLParameter = function(name) {
 		return decodeURIComponent((new RegExp('[?|&]' + name + '=' + '([^&;]+?)(&|#|;|$)').exec(location.search) || [null, ''])[1].replace(/\+/g, '%20')) || null;
    	};
-
+/**------------- 2.2.5 Focus / Scroll to an Element -------------**/
+	window.scrollToanElement = function(element) {
+		$('html, body').animate({ scrollTop: $(element).offset().top }, 'slow');
+	};
 } )( jQuery );
