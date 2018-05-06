e131ed69470b88ca51c619a6bc66313113cae1bd
LAPTOP-1SS5URD1\Jun <lamsmallarijr@gmail.com>
2018-05-06 11:59:10 +0800

    added vc_map sample shortcode

--- shortcodes/sample-shortcode.php
+++ shortcodes/sample-shortcode.php
@@ -30,3 +30,31 @@
 	}
 	add_shortcode( 'product-slider' , 'productSlider' );
 
+	// ______________________________ =Home Special Blocks ______________________________
+	function homeSmallBlocks( $atts ) {
+		$sc_atts = shortcode_atts(
+	                array(
+	                    'box_image' => '',
+	                    'background_color' => '',
+	                    'title' => '',
+	                    'excerpt' => '',
+	                    'link' => ''
+	                ), $atts, 'home-small-blocks' );
+
+		ob_start();
+	    ?>
+	    	<div class="hsb-one-thirds__single__container">
+				<div class="hsb-one-thirds__single" style="background-color: <?php echo $sc_atts['background_color'] ?>">
+					<div class="hsb-one-thirds__image" style="background-image: url(<?php echo wp_get_attachment_url($sc_atts['box_image']) ?>);"></div>
+					<h4 class="hsb-one-thirds__single__title"><span><em><?php echo $sc_atts['title'] ?></em></span></h4>
+					<div class="hsb-slide-down-details">
+						<div class="hsb-one-thirds__single__excerpt"><?php echo $sc_atts['excerpt'] ?></div>
+						<a href="<?php echo $sc_atts['link'] ?>" class="hsb-one-thirds__single__premalink">Read More<span> >></span></a>
+					</div>
+				</div>
+			</div>
+		<?php
+		return ob_get_clean();
+	}
+	add_shortcode( 'home-small-blocks' , 'homeSmallBlocks' );
+	// [home-small-blocks box_image="" background_color="" title="" excerpt="" link="" ]
